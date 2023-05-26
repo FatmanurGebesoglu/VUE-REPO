c58fa1321dfc441067c35a06e1b39f119a804ac0
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-26 12:27:41 +0300

    Header component created

---
+++ 6-CLI-APP/cli/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ baslik }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            baslik: 'Merhaba Vue',
+        };
+    }
+}
+
+
+</script>
+
+<style></style>
