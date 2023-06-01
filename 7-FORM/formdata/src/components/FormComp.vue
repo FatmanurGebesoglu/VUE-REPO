4c5502b077853ff4c3e1531859783f3070069a25
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-06-01 11:05:07 +0300

    databinding with select

--- 7-FORM/formdata/src/components/FormComp.vue
+++ 7-FORM/formdata/src/components/FormComp.vue
@@ -2,15 +2,23 @@
     <form>
         <label>Email:</label>
         <input type="email" v-model="email" />
-        {{ email  }}
+        <br>
+        <br>
+        <label>Yetki</label>
+        <select v-model="yetki">
+            <option value="1">Admin</option>
+            <option value="2">User</option>
+        </select>
+        {{ yetki }}
     </form>
 </template>
-
+//value ile option yapıldığında yeki value olacak eğer value olmazsa option içindeki değer value olacak
 <script>
     export default{
         data(){
             return{
-                email: ''
+                email: '',
+                yetki: '1'
             }
         },
     }
