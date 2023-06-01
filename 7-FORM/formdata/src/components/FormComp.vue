91732c8af75e31d93d275e8a1213f27d86008403
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-06-01 11:22:03 +0300

    databinding with checkbox

--- 7-FORM/formdata/src/components/FormComp.vue
+++ 7-FORM/formdata/src/components/FormComp.vue
@@ -10,6 +10,29 @@
             <option value="2">User</option>
         </select>
         {{ yetki }}
+        <hr/>
+        <br>
+        <br>
+        <input type="checkbox" v-model="anlasma" />
+        <label>kabul ediyorum</label>
+        <hr/>
+        <p>Anlaşma Kabulu {{ anlasma ? 'Ediyorum': 'Etmiyorum'}}</p>
+        <hr/>
+        <div>
+            <input type="checkbox" value="yönetim" v-model="istekler"/>
+            <label>Yönetim</label>
+        </div>
+        <div>
+            <input type="checkbox" value="erişim" v-model="istekler"/>
+            <label>Erişim</label>
+        </div>
+        <div>
+            <input type="checkbox" value="silme" v-model="istekler"/>
+            <label>Silme</label>
+        </div>
+        <p>İstekler: {{ istekler }}</p>
+
+        
     </form>
 </template>
 //value ile option yapıldığında yeki value olacak eğer value olmazsa option içindeki değer value olacak
@@ -18,7 +41,9 @@
         data(){
             return{
                 email: '',
-                yetki: '1'
+                yetki: '1',
+                anlasma: false,
+                istekler: []
             }
         },
     }
