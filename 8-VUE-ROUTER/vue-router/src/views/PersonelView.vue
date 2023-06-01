9cb01fdd35ee08101004aff965fda85f44b10154
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-06-01 15:32:03 +0300

    routing

---
+++ 8-VUE-ROUTER/vue-router/src/views/PersonelView.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Personeller</h1>
+    <div v-for="personel in personeller" :key="personel.id">
+        <p>{{ personel.ad }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonelView',
+  data () {
+    return {
+        personeller : [
+            {id:1, ad:'Ali', gorev:'Muhasebe'},
+            {id:2, ad:'Veli', gorev:'İnsan Kaynakları'},
+            {id:3, ad:'Ayşe', gorev:'Satın Alma'},
+        ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
