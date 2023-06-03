a72b97c9a4ca682c782c70fa33948a550a413eb4
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-06-03 17:50:24 +0300

    reactive temp

--- 9-COMPOSITION-APP/comp-app/src/views/HomeView.vue
+++ 9-COMPOSITION-APP/comp-app/src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
-    <p>{{ name }}--{{ surname }}</p>
+    <!-- <p>{{ name }}--{{ surname }}</p>
+    <button @click="buttonTiklandi">Tıkla</button> -->
+
     <button @click="buttonTiklandi">Tıkla</button>
+    <p>{{ ad }}-- {{ yetenekNesnesi.yetenek }}</p>
+    <input type="text" v-model="ad" />
+
   </div>
 </template>
 
@@ -11,18 +16,48 @@
 //önce setup çalışır daha sonra created en son mounted çalışır
 //setup bütün bütün fonksiyonların üzerinde bir fonksiyondur o yüzden önce o çalışır
 //setup içindeki değişkenler kullanılmak için return ile döndürülmelidir
+
+
+import { ref, reactive } from 'vue';
+
 export default {
   name: 'HomeView',
   setup() {
-    console.log('setup yüklendi')
+    // console.log('setup yüklendi')
 
-    let name = 'Mehmet';
-    let surname = 'Kara';
+    // let name = 'Mehmet';
+    // let surname = 'Kara';
+
+    // const buttonTiklandi=()=>{
+    //   console.log('tıklandı')
+    // }
+    // return { name, surname, buttonTiklandi }
+
+    // const bilgiler =ref(null);
+
+    // const buttonTiklandi=()=>{
+    //   console.log(bilgiler, bilgiler.value);
+    // }
+    
+    //reactive genellikle nesneler için kullanılır
+    //ref ise string, number, boolean için kullanılır
+
+    let ad = ref("Fatmanur");
+    //let yetenek = reactive("Yemek yapmak"); ====>>>> bu şekilde kullanılmaz!
+
+
+    let yetenekNesnesi = reactive({
+      yetenek: "Yemek yapmak"
+    });
 
     const buttonTiklandi=()=>{
-      console.log('tıklandı')
+      ad.value="Mehmet";
+      yetenekNesnesi.yetenek="Kod yazmak";
     }
-    return { name, surname, buttonTiklandi }
+
+
+    return { ad, buttonTiklandi, yetenekNesnesi }
+
   },
   mounted() {
     console.log('onMounted yüklendi')
