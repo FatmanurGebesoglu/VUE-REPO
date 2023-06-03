2af7880adf206df633913e9a397fd99755ac987f
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-06-03 17:24:09 +0300

    setup func.

---
+++ 9-COMPOSITION-APP/comp-app/src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <p>{{ name }}--{{ surname }}</p>
+    <button @click="buttonTiklandi">Tıkla</button>
+  </div>
+</template>
+
+<script>
+
+
+//önce setup çalışır daha sonra created en son mounted çalışır
+//setup bütün bütün fonksiyonların üzerinde bir fonksiyondur o yüzden önce o çalışır
+//setup içindeki değişkenler kullanılmak için return ile döndürülmelidir
+export default {
+  name: 'HomeView',
+  setup() {
+    console.log('setup yüklendi')
+
+    let name = 'Mehmet';
+    let surname = 'Kara';
+
+    const buttonTiklandi=()=>{
+      console.log('tıklandı')
+    }
+    return { name, surname, buttonTiklandi }
+  },
+  mounted() {
+    console.log('onMounted yüklendi')
+  },
+  created() {
+    console.log('created yüklendi')
+  },
+}
+
+</script>
